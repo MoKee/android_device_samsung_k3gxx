df91a7953921fd19e63c7493d85239f52179ce50
Afshin Arani <afshinarani79@gmail.com>
2017-01-13 22:04:08 +0330

    k3gxx: mokee-ify
    Change-Id: Ie1c297a9d4384f183d381a6f1f01f85e75dfe8f8

--- mokee.mk
+++ mokee.mk
@@ -1,7 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := k3gxx
 
-# Inherit some common Lineage stuff.
+# Inherit some common MK stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/k3gxx/device.mk)
 $(call inherit-product, device/samsung/k3gxx/system_prop.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_k3gxx
+PRODUCT_NAME := mk_k3gxx
 PRODUCT_DEVICE := k3gxx
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-G900H
